6a89f8aa2f4a0af7de5e9fb448789a78b4b6bf2f
RajniHarsha27 <145461905+RajniHarsha27@users.noreply.github.com>
2025-01-29 14:53:06 +0530

    Update models.py

--- models.py
+++ models.py
@@ -2,7 +2,15 @@ from pydantic import BaseModel
 from typing import List, Optional
 
 class QuestionSchema(BaseModel):
+    email: str  # New email field
     jd_text: str  # Job description text
     sample_questions: Optional[str] = None  # Optional sample questions
     cv_content: str  # CV content extracted from the file
     questions: List[str]  # List of generated questions
+
+class Question_AnswerSchema(BaseModel):
+    email: str  # New email field
+    
+    questions: List[str]  # List of generated questions
+    Answers: List[str]  # List of generated answers
+
